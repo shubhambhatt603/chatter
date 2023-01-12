5946046705f2400a3a0c65a9f06c571df1d8735a
unknown <shubhambhatt603@gmail.com>
2023-01-12 11:17:12 +0530

    init project

---
+++ components/realtime-messages.tsx
@@ -0,0 +1,35 @@
+import { json } from "@remix-run/node";
+import { useOutletContext } from "@remix-run/react";
+import type { Database } from "db_types";
+import { useEffect, useState } from "react";
+import { SupabaseOutletContext } from "~/root";
+
+type Message = Database["public"] ["Tables"] ["messages"] ["Row"];
+
+export default function RealtimeMessages({
+    serverMessages,
+}: {
+    serverMessages: Message[];
+}) {
+    const [messages, setMessages] = useState(serverMessages)
+    const {supabase} = useOutletContext<SupabaseOutletContext>();
+
+    useEffect(() => {
+        setMessages(serverMessages);
+    }, [serverMessages]);
+
+    useEffect(() => {
+        const channel = supabase.channel('*').on('postgres_changes', {event: 'INSERT', schema: 'public', tables: 'messages'}, (payload: any) => {
+            const newMessage = payload.new as Message
+
+            if (!messages.find(message => message.id === newMessage.id)) {
+                setMessages([...messages, newMessage]);
+            }
+        }).subscribe()
+
+        return () => {
+            supabase.removeChannel(channel);
+        }
+    }, [supabase, messages, setMessages]);
+    return <pre>{JSON.stringify(messages, null, 2)}</pre>
+} 
